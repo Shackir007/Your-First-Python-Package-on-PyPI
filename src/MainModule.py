dd72acab55c73bee6d50bce6e61203e00b998fff
Shackir007 <38275910+Shackir007@users.noreply.github.com>
2024-01-30 00:37:53 +0530

    Update MainModule.py

--- src/MainModule.py
+++ src/MainModule.py
@@ -1,7 +1,7 @@
 __author__='Your Name Goes here'
 __author_email__='Email of the point of contact'
 
-```
+'''
 Define your code here.
 
 e.g.:
@@ -12,7 +12,7 @@ def your_1st_function():
     
     return ...
 
-```
+'''
 def add_one(number):
     print("Message from Packagingtutorial")
     return number + 1
